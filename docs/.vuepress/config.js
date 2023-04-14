fb0db231698e782e2ac9dd0de5e8acb9b72e6601
JUSTIN <584058486@qq.com>
2023-04-15 01:43:05 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  themeConfig: {
+    sidebar: [
+      '/',
+			{
+        title: 'PROMISE',   // 必要的
+        path: '/promise',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+      },
+			{
+        title: '搭建react项目',
+        path: '/react/create', 
+      },
+			{
+				title: 'js包管理',
+				path: '/node/package_manage'
+			}
+    ]
+  }
+}
---
+++ docs/react/create.md
@@ -0,0 +1,3 @@
+# 使用webpack搭建一个react项目
+1. 创建一个文件夹，如my_react
+2. 执行npm
